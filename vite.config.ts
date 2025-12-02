d0e4bae4657db939e5501502237b8f5c863e1789
tyl-create <bluetone8210@gmail.com>
2025-12-02 09:30:51 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 把下面的 '你的專案名稱' 改成你 GitHub Repository 的名字
-  // 例如你的網址是 github.com/user/electro-lab，這裡就填 '/electro-lab/'
-  base: '/elec_n/', 
+  base: './',
 })
